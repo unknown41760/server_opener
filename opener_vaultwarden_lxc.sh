ef7b076abbb87405e7cd43c053b28724aa158ec7
unknown41760 <130830821+unknown41760@users.noreply.github.com>
2026-02-20 15:29:27 +0300

    Update opener_vaultwarden_lxc.sh

--- opener_vaultwarden_lxc.sh
+++ opener_vaultwarden_lxc.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+
 echo "=== Vaultwarden LXC Hardening ==="
 
 # Make a new user instead of root
